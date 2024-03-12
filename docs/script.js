d69a280017b64b96a05fa87c90f346854e3b8c20
るーしー <127585375+rucykun@users.noreply.github.com>
2024-03-13 00:56:00 +0900

    Create script.js

---
+++ docs/script.js
@@ -0,0 +1,10 @@
+function nick(){
+  document.getElementById("username").innerHTML = document.getElementById('my1').value
+}
+function messages(){
+  document.getElementById("content").innerHTML = document.getElementById('my2').value
+}
+function av(){
+  var image = document.getElementById("avv");
+  image.src = document.getElementById('my3').value;
+}
